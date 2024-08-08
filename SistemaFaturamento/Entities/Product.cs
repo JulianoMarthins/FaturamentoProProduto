7ed30e966edc2a9df4e0062e9638c3055ec5b335
JulianoMarthins <JulianoMarthins@Yahoo.com.br>
2024-08-07 23:16:55 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaFaturamento/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿
+namespace SistemaFaturamento.Entities
+{
+    internal class Product
+    {
+
+        // Atributos 
+        public string name { get; set; }
+        public double price { get; set; }
+        public int quantity { get; set; }
+
+
+        // Construtor
+        public Product(string name, double price, int quantity)
+        {
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+
+
+        // Metodos
+        public double total()
+        {
+            return this.price * this.quantity;
+        }
+
+        public override string ToString()
+        {
+            string mensagem = $"Name: {this.name}\nPrice: ${this.price}\nQuantity: {this.quantity}";
+            return mensagem;
+        }
+    }
+}
+
---
+++ SistemaFaturamento/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using SistemaFaturamento.Entities;
+
+namespace SistemaFaturamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = @"D:\WorkSpace\ArquivoOrigem.txt";
+            string targetPath = @"D:\WorkSpace\Out\summary.txt";
+
+            List<Product> product = new List<Product>();
+
+
+            try
+            {
+                using (StreamReader stream = File.OpenText(path))
+                {
+                    int contador = 0;
+                    while (!stream.EndOfStream)
+                    {
+                        string line = stream.ReadLine();
+
+                        string[] vetor = line.Split(',');
+
+                        string name = vetor[0];
+                        double price = double.Parse(vetor[1].Replace('.', ','));
+                        int quantity = int.Parse(vetor[2]);
+
+                        product.Add(new Product(name, price, quantity));
+                        contador++;
+                    }
+                }
+
+                using (StreamWriter novoArquivo = File.AppendText(targetPath))
+                {
+                    foreach (Product produto in product)
+                    {
+                        double total = produto.total();
+                        novoArquivo.WriteLine(produto.name + "," + total);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An Error Occured!\n " + e.Message);
+            }
+
+            Console.WriteLine("Program completed successfully");
+        }
+    }
+}
+
